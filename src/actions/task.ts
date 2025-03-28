1a7945c25c087461aa7c920d21823cee65d076c8
tsunasan-html <>
2025-03-28 12:03:57 +0900

    fixed

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -20,7 +20,7 @@ export const createTask = async (state: FormState, formData: FormData) => {
   try {
     await connectDb()
     await TaskModel.create(newTask)
-  } catch (error) {
+  } catch (_error) {
     state.error = "タスクの作成に失敗";
     return state;
   }
@@ -40,7 +40,7 @@ export const updateTask = async (id: string, state: FormState, formData: FormDat
   try {
     await connectDb()
     await TaskModel.updateOne({_id: id }, updateTask);
-  } catch (error) {
+  } catch (_error) {
     state.error = "タスクの更新に失敗";
     return state;
   }
@@ -51,7 +51,7 @@ export const deleteTask = async ( id: string, state: FormState) => {
   try {
     await connectDb()
     await TaskModel.deleteOne({_id: id });
-  } catch (error) {
+  } catch (_error) {
     state.error = "タスクの削除に失敗";
     return state;
   }
